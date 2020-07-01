680a1d2232551964ed92be4d5908d37583bbc461
David-Kopczynski <david.kop.dk@gmail.com>
2020-07-01 22:14:21 +0200

    v0.1 First stable version -- main and defuser + Planning and HowToUse

---
+++ Arduino/Defuser/Defuser.ino
@@ -0,0 +1,77 @@
+/* ---------- Display ---------- */
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+
+/* ---------- Functions ---------- */
+void updateDisplay(String text, int line = 0);
+
+
+/* ---------- Runtime ---------- */
+String code;
+unsigned long duration;
+
+unsigned long startTime;
+unsigned int charIndex = 1;
+
+
+void setup() {
+  
+  // Initial
+  Serial.begin(9600); 
+
+  // Turn on the blacklight and print a message.
+  lcd.begin();
+  updateDisplay("Defuse Code:");
+  lcd.backlight();
+
+  // Request communication
+  Serial.write("Request code");
+}
+
+void loop() {
+
+  // Get Data
+  while (Serial.available()) {
+
+    String input = Serial.readString();
+
+    // Get code
+    if (code.length() == 0) {
+      code = input;
+
+      // Next request
+      Serial.write("Request time");
+    }
+    else if (duration == 0) {
+      duration = input.toInt();
+
+      // Set start values 
+      startTime = millis();
+
+      // Write placeholder
+      for (int i = 0; i < code.length(); i++) {
+        lcd.setCursor(i, 1);
+        lcd.print("-");
+      }
+    }
+  }
+
+  // Display Code after time
+  if (startTime && duration && charIndex <= code.length() && duration * charIndex / code.length() < millis() - startTime) {
+
+    // Display next
+    lcd.setCursor(charIndex -1, 1);
+    lcd.print(code[charIndex -1]);
+
+    charIndex += 1;
+  }
+}
+
+void updateDisplay(String text, int line = 0) {
+  lcd.setCursor(0, line);
+
+  while (text.length() < 16) text += " ";
+  lcd.print(text);
+}
---
+++ Arduino/HowToUse.md
@@ -0,0 +1,22 @@
+When starting all essential values have been declared for speedsoft:
+-Timer at 40 seconds 
+-Defuser at 3 seconds 
+-Code length at 6 characters
+-One input error
+-And as a result, no penalty
+
+When speedsoft is activated you can simply put in your code or press start to apply a random code 
+When speedsoft is deactivated you can overwrite all values and play as you want:
+-Time in minutes
+-Defuser in seconds
+-Allowed input errors or empty for unlimited
+-Penalty for typing errors, if the value has been set 
+-A custom code up to 16 characters 
+
+Then the game starts and the clock starts ticking down, beeping every second faster than before
+
+To defuse the bomb you need to plug in the defuse kit "Defuser" and wait 
+-when all chars have been revealed you can put in the code and win
+
+If the time runs out the terrorists win and the game is over
+If a value for errors is set and has been reached the terrorists win and the game is over
